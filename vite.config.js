cc1c74986270518750b71625ff3d9e2a2eabe02f
Veaceslav Kuhn <grigorov.veacheslav@gmail.com>
2025-08-14 13:08:48 +0200

    no message

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+	base: "/webaudio/", // Set the base path for GitHub Pages
 	server: {
 		port: 3000,
 		open: true,
